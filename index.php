43b937da0afd164d5d6f52622574bad98754a17e
willyammach <willyam.mach@gmail.com>
2024-07-28 16:13:31 -0300

    create user auth api

--- index.php
+++ index.php
@@ -1,28 +1,44 @@
 |<?php 
-    $data = [];
+    header("Content-Type: application/json");
 
-    // requisição
-    if(isset($_GET['option'])) {
-        switch ($_GET['option']) {
-            case 'status':
-                $data['status'] = 'SUCCESS';
-                $data['data'] = 'API running';
-                break;
-            
-            default: 
-                $data['status'] = 'ERROR';
-                break;
+    $users = [
+        [
+            "id" => 0,
+            "username" => "Willyam",
+            "password" => "123" 
+        ],
+        [
+            "id" => 1,
+            "username" => "Cesar",
+            "password" => "123" 
+        ],
+        [
+            "id" => 2,
+            "username" => "Marcos",
+            "password" => "123" 
+        ]
+    ];
+
+    echo json_encode($users);
+
+    $data = json_decode(file_get_contents("php://input"), true);
+
+    if($_SERVER['REQUEST_METHOD'] === "POST") {
+        if(isset($data['username']) && isset($data['password'])) {
+            $username = $data['username'];
+            $password = $data['password'];
+
+            for($i = 0; $i < count($users); $i++) {
+                if($users[$i]['username'] == $username && $users[$i]['password'] == $password) {
+                    echo json_encode([
+                        'status' => 'success',
+                        'mensage' => 'Login successful'
+                    ]);
+                } else {
+                    echo json_encode(['status' => 'Error', 'mensage' => 'User or passoword invalid']);
+                }
+            }
         }
-    } else {
-        $data['status'] = 'ERROR';
-    }
-
-    // msoitrar a resposta da api  
-    response($data);
-
-    function response($data) {
-        header("Content-Type:application/json");
-        echo json_encode($data);
     }
 
     // montar a resposta
